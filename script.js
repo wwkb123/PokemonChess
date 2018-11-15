27c38f58e7806f010e9475b2de2bc54c91cc44bf
Yiu Chung Yau <yiuchungyau@gmail.com>
2018-11-15 00:56:24 -0500

    add grids

--- script.js
+++ script.js
@@ -1,18 +1,53 @@
 function setup() { //initialize everything
-  fillFunctionButtons();
-  fillStatusText();
-  fillProgressBar();
   fillMatrix();
-  setStatusText("Loaded succesfully!", "text-bold" );
-}
-
-function fillFunctionButtons() {
   var headDiv = document.getElementById("head");
   var funcBtnRow = createRow();
-  // createButton(buttonText, styleClass, functionName);
-  funcBtnRow.appendChild(createButton("All Mid All Random", "btn btn-primary btn-sm m-3", "f1()"));
-  funcBtnRow.appendChild(createButton("Drop the beat", "btn btn-warning btn-sm m-3", "f2()"));
-  funcBtnRow.appendChild(createButton("Defile", "btn btn-dark btn-sm m-3", "f3()"));
-  funcBtnRow.appendChild(createButton("Puuurrrge!", "btn btn-light m-3", "f4()"));
   headDiv.appendChild(funcBtnRow);
+}
+
+function fillMatrix() {
+  var matrix = document.getElementById("grid");
+  for (i = 0; i < 8; i++) {
+    var newRow = createRow("justify-content-md-center");
+    for (j = 0; j < 8; j++) {
+      newRow.appendChild(createDefaultButton(i, j));
+    }
+    matrix.appendChild(newRow);
+  }
+}
+
+function createDefaultButton() {
+  var button = document.createElement("div");
+  button.className = "thumbnail";
+  button.setAttribute("onclick", "buttonClicked("+i+","+j+")");
+
+  //the image part
+  var img = document.createElement("img");
+  img.id = "img_" + i + "_" + j;
+  img.setAttribute("src", "images/grid.jpg");
+  img.setAttribute("alt", "white");
+  img.setAttribute("width", "75");
+  img.setAttribute("height", "75");
+
+  //the text part
+  var text = document.createElement("label");
+  text.setAttribute("class", "caption unselectable");
+  text.id = "text_" + i + "_" + j;
+
+  button.appendChild(img);
+  button.appendChild(text);
+  return button;
+}
+
+
+// helper functions below
+
+function createRow(className) {
+  var rowDiv = document.createElement("div");
+  if (className == null) {
+    rowDiv.className = "row";
+  } else {
+    rowDiv.className = "row " + className;
+  }
+  return rowDiv;
 }
