097fc59ac7dc27e75ea28c5a287b58346f1f990e
Yiu Chung Yau <yiuchungyau@gmail.com>
2018-12-05 02:23:09 -0500

    add disclaimer

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 /*
 
+
+  Disclaimer:
+  This game is created by a Pokemon enthusiast, and it is for educational purpose only.
+  Any of the trademarks, service marks, collective marks, design rights, personality rights,
+  or similar rights that are mentioned, used, or cited in this game are the property of their respective owners.
+
+
+*/
+
+
+/*
+
 	 
         in this program,
           - - - - - - - -> j
